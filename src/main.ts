386898acceb72a1c7ed91170680ea6133e196b1d
exbinario <miguel.4to.1994@gmail.com>
2022-11-20 00:01:47 -0600

    cambios menores

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//hola
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
