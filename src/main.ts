df96d9427f316627b01f45bc5a33718e99c8c6ea
exbinario <miguel.4to.1994@gmail.com>
2022-11-21 09:44:18 -0600

    test ssl5

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/privkey.pem'),
-  //   cert: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/cert.pem'),
-  // };
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors();
-  await app.listen(parseInt(process.env.PORT) || 3000);
-  // await app.listen(443);
+  // await app.listen(parseInt(process.env.PORT) || 3000);
+  await app.listen(443);
 }
 bootstrap();
