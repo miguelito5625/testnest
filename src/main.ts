46422302d01a316a769ca24e421b6c408583e56c
exbinario <miguel.4to.1994@gmail.com>
2022-11-21 06:11:39 -0600

    prueba deploy34

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
