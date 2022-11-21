8febb0ca428e863d4c2f0a661693bf3b6d7805c7
exbinario <miguel.4to.1994@gmail.com>
2022-11-21 09:30:41 -0600

    test ssl2

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import fs from 'fs';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('./secrets/private-key.pem'),
-    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/cert.pem'),
   };
   const app = await NestFactory.create(AppModule);
   app.enableCors();
