6b4bc8d70d794f86cbfae8b0719bf6bb8cfb33db
exbinario <miguel.4to.1994@gmail.com>
2022-11-21 09:28:36 -0600

    test ssl

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fs from 'fs';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(parseInt(process.env.PORT) || 3000);
+  // await app.listen(parseInt(process.env.PORT) || 3000);
+  await app.listen(443);
 }
 bootstrap();
