274d556831678d31cd7976bf987636510aad4425
exbinario <miguel.4to.1994@gmail.com>
2022-11-22 18:27:43 -0600

    corriendo para el desarrollo

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/cert.pem'),
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  // const httpsOptions = {
+  //   key: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/privkey.pem'),
+  //   cert: fs.readFileSync('/etc/letsencrypt/live/mikeserver1.tk/cert.pem'),
+  // };
+  // const app = await NestFactory.create(AppModule, {
+  //   httpsOptions,
+  // });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  // await app.listen(parseInt(process.env.PORT) || 3000);
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
